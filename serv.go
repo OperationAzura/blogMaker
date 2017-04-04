7e2bac963cc08e13762b36d9ad6459c2d37bb4bd
Derek <derekstich@gmail.com>
2017-04-03 20:57:19 -0400

    stuff

--- serv.go
+++ serv.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os/exec"
 		"time"
 	"os"
 	"bytes"
@@ -42,6 +43,22 @@ func saveHandler(w http.ResponseWriter, req *http.Request) {
 	}
 	ctx.Date = time.Now().Format("2017-03-18T22:18:08-05:00")
 
+
+		if ctx.Image != ""{
+			if ctx.Image[4:] == "http"{
+				cmd := exec.Command("wget", "-O", "./static/images/"+ctx.Title+".jpg", ctx.Image)
+				err := cmd.Start()
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Printf("Waiting for command to finish...")
+				err = cmd.Wait()
+				if err != nil{
+					fmt.Printf("Command finished with error: %v", err)
+				}
+				ctx.Image = ctx.Title+".jpg"
+			}
+		}
 
 	t, err := template.ParseFiles("./blogTemplate.t")
 	if err != nil {
